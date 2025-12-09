5bd862d5fe1f3b3f1c35917faf00f99d9345bd1f
Vincent Batallones <202310567@gordoncollege.edu.ph>
2025-12-09 10:08:22 +0800

    button transparency on budget overview fixed

--- be/fe/src/pages/admin/budgets/AddBudgets.tsx
+++ be/fe/src/pages/admin/budgets/AddBudgets.tsx
@@ -24,7 +24,7 @@ const AddBudgets = () => {
 
   const currentYear = new Date().getFullYear();
   const yearOptions = Array.from(
-    { length: currentYear - 2000 + 1 },
+    { length: currentYear - 2000 + 2 },
     (_, i) => currentYear - i
   ).filter((year) => !years.includes(year));
 
